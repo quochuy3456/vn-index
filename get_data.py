c7a3eba8c618b6385658247fd8b36923e80315e2
Do Huy 1 <huy.do1@ntq-solution.com.vn>
2020-06-08 16:42:29 +0700

    add get data

--- get_data.py
+++ get_data.py
@@ -4,38 +4,63 @@ import os
 import logging
 import configparser
 
-pth = "https://s.cafef.vn/hose/"
-company_name = "VCB-ngan-hang-thuong-mai-co-phan-ngoai-thuong-viet-nam.chn"
+pth = "https://s.cafef.vn/"
+state = "hastc"
+company_name = "HDA-cong-ty-co-phan-hang-son-dong-a.chn"
 
 config = configparser.ConfigParser()
 config.read('config.cfg')
 
 
 class GetIndex:
-    def __init__(self, _path, cpn_name):
+    def __init__(self, _path, state, cpn_name):
         self.path = _path
+        self.state = state
         self.cpn_name = cpn_name
         self.all_data = None
         self.get_data()
 
     def get_full_path(self):
-        return os.path.join(self.path + self.cpn_name)
+        return os.path.join(self.path, self.state, self.cpn_name)
 
     def get_data(self):
         _data = requests.get(self.get_full_path())
         self.all_data = BeautifulSoup(_data.text, "html.parser")
 
-    def get_eps_via_class(self):
-        eps = self.all_data.find("div", {"class": "tidown"})
-        lst_val = eps.find('ul').find_all("li")
-        outcome = []
-        for o in lst_val:
-            outcome.append(list(map(lambda x: x.text, o.find_all("span"))))
-        return outcome
+    def get_index_value_group_style(self):
+        info = self.all_data.find("div", {"class": "dl-thongtin clearfix"})
+        data = info.find_all("ul")
+        print(data)
+
+    def get_index_value(self):
+        info = self.all_data.find("div", {"class": "tidown"})
+        if info:
+            lst_val = info.find('ul').find_all("li")
+            index_value = []
+            for o in lst_val:
+                index_value.append(list(map(lambda x: x.text, o.find_all("span"))))
+            return index_value
+        else:
+            return None
+
+    def get_cpn_status(self):
+        info = self.all_data.find("div", {"id": "divHoSoCongTyAjax"})
+        table = info.find('table')
+        rows = table.find_all('tr')
+        data_info = []
+        for row in rows:
+            cols = row.find_all('td')
+            cols = [ele.text.strip() for ele in cols]
+            data_info.append([ele for ele in cols if ele])
+        data_info = list(filter(lambda x: True if len(x) >= 4 else False, data_info))
+        return data_info[:]
 
 
-data = GetIndex(pth, company_name)
+data = GetIndex(pth, state, company_name)
 
-d = data.get_eps_via_class()
+b = data.get_index_value_group_style()
+e = data.get_index_value()
+d = data.get_cpn_status()
 
+print(e)
 print(d)
