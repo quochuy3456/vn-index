63bfa2248155467649917efc6907dc106213c42a
nguyen tuan hung <hung.nguyen2@ntq-solution.com.vn>
2020-06-09 17:50:12 +0700

    edit ajax

---
+++ clone_data_ajax.py
@@ -0,0 +1,80 @@
+import requests
+from bs4 import BeautifulSoup
+
+path = "https://s.cafef.vn/Ajax/bank/BHoSoCongTy.aspx?symbol=VCB&Type=2&PageIndex=0&PageSize=4&donvi=1"
+
+path = "https://s.cafef.vn/Ajax/Bank/BHoSoCongTy.aspx?symbol=BID&Type=1&PageIndex=0&PageSize=4&donvi=1"
+# path = "https://s.cafef.vn/Ajax/HoSoCongTy.aspx?symbol=vnm&Type=2&PageIndex=0&PageSize=4"
+#
+# path = "https://s.cafef.vn/Ajax/HoSoCongTy.aspx?symbol=hhc&Type=2&PageIndex=0&PageSize=4"
+
+data = requests.get(path)
+
+print(data.text)
+
+path = "https://s.cafef.vn/Lich-su-giao-dich-BID-1.chn"
+
+data = {
+'ctl00$ContentPlaceHolder1$scriptmanager': 'ctl00$ContentPlaceHolder1$ctl03$panelAjax|ctl00$ContentPlaceHolder1$ctl03$pager2',
+'__EVENTTARGET': 'ctl00$ContentPlaceHolder1$ctl03$pager2',
+'__EVENTARGUMENT': 1,
+'__VIEWSTATE': '/wEPDwUKMTU2NzY0ODUyMGQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgEFKGN0bDAwJENvbnRlbnRQbGFjZUhvbGRlcjEkY3RsMDMkYnRTZWFyY2jJnyPYjjwDsOatyCQBZar0ZSQygQ==',
+'ctl00$ContentPlaceHolder1$ctl03$txtKeyword': 'BID',
+'ctl00$ContentPlaceHolder1$ctl03$dpkTradeDate1$txtDatePicker':'',
+'ctl00$ContentPlaceHolder1$ctl03$dpkTradeDate2$txtDatePicker':'',
+'ctl00$UcFooter2$hdIP':'',
+'__VIEWSTATEGENERATOR': '2E2252AF',
+'__ASYNCPOST': 'true'
+}
+
+a = requests.post(path,
+                  data=data,
+                  headers={
+                      "Accept": "*/*",
+                      "Accept-Encoding": "gzip, deflate, br",
+                      "Accept-Language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
+                      "Cache-Control": "no-cache",
+                      "Connection": "keep-alive",
+                      "Content-Length": "655",
+                      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                      "Host": "s.cafef.vn",
+                      "Origin": "https://s.cafef.vn",
+                      "Referer": "https://s.cafef.vn/Lich-su-giao-dich-BID-2.chn",
+                      "Sec-Fetch-Dest": "empty",
+                      "Sec-Fetch-Mode": "cors",
+                      "Sec-Fetch-Site": "same-origin",
+                      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+                      "X-MicrosoftAjax": "Delta=true",
+                  }
+                  )
+
+print(a.text)
+
+class GetIndex:
+    def __init__(self, path, cpn_code, category=None):
+        self.path = path
+        self.category = category
+        self.cpn_code = cpn_code
+        self.all_data = None
+        self.clone_data()
+
+    def clone_data(self):
+        """
+            Clone data from ajax link
+        """
+        if self.category:
+            _path = f'https://s.cafef.vn/Ajax/{self.category}/BHoSoCongTy.aspx?symbol={self.cpn_code}&Type=2&PageIndex=0&PageSize=4&donvi=1'
+            data = requests.get(_path)
+        else:
+            _path = f'https://s.cafef.vn/Ajax/BHoSoCongTy.aspx?symbol={self.cpn_code}&Type=2&PageIndex=0&PageSize=4'
+            data = requests.get(_path)
+        self.all_data = BeautifulSoup(data.text, "html.parser")
+
+    def get_cnp_status(self):
+        tbl = self.all_data.find('table')
+        # rows = tbl.findAll('tr')
+        return tbl
+
+
+# getdata = GetIndex(path=None, cpn_code="VNM")
+# print(getdata.get_cnp_status())
--- get_data.py
+++ get_data.py
@@ -4,12 +4,16 @@ import os
 import logging
 import configparser
 
-pth = "https://s.cafef.vn/"
-state = "hastc"
-company_name = "HDA-cong-ty-co-phan-hang-son-dong-a.chn"
+pth = "https://s.cafef.vn"
+state = "upcom"
+company_name = "MNB-tong-cong-ty-may-nha-be-cong-ty-co-phan.chn"
 
-config = configparser.ConfigParser()
-config.read('config.cfg')
+# config = configparser.ConfigParser()
+# config.read('config.cfg')
+
+data = requests.get("https://s.cafef.vn/Ajax/Bank/BHoSoCongTy.aspx?symbol=VCB&Type=1&PageIndex=0&PageSize=4&donvi=1")
+"https://s.cafef.vn/Ajax/Bank/BHoSoCongTy.aspx?symbol=VCB&Type=2&PageIndex=0&PageSize=4&donvi=1000"
+print(data.text)
 
 
 class GetIndex:
@@ -29,19 +33,30 @@ class GetIndex:
 
     def get_index_value_group_style(self):
         info = self.all_data.find("div", {"class": "dl-thongtin clearfix"})
-        data = info.find_all("ul")
-        print(data)
+        print(info)
+        data = info.find_all("ul")[-1]
+        li = data.find_all('li')
+        dta = []
+        for o in li:
+            dta.append(list(map(lambda x: x.get_text(strip=True), o.find_all("div"))))
+        return dta
+
+    def get_index_value_list_st(self):
+        info = self.all_data.find("div", {"class": "tidown"})
+        lst_val = info.find('ul').find_all("li")
+        print(lst_val)
+        index_value = []
+        for o in lst_val:
+            index_value.append(list(map(lambda x: x.get_text(strip=True), o.find_all("span"))))
+        return index_value
 
     def get_index_value(self):
-        info = self.all_data.find("div", {"class": "tidown"})
-        if info:
-            lst_val = info.find('ul').find_all("li")
-            index_value = []
-            for o in lst_val:
-                index_value.append(list(map(lambda x: x.text, o.find_all("span"))))
-            return index_value
-        else:
-            return None
+        try:
+            index_value = self.get_index_value_group_style()
+        except:
+            index_value = self.get_index_value_list_st()
+
+        return index_value
 
     def get_cpn_status(self):
         info = self.all_data.find("div", {"id": "divHoSoCongTyAjax"})
@@ -53,14 +68,13 @@ class GetIndex:
             cols = [ele.text.strip() for ele in cols]
             data_info.append([ele for ele in cols if ele])
         data_info = list(filter(lambda x: True if len(x) >= 4 else False, data_info))
-        return data_info[:]
+        return data_info
 
 
-data = GetIndex(pth, state, company_name)
-
-b = data.get_index_value_group_style()
-e = data.get_index_value()
-d = data.get_cpn_status()
-
-print(e)
-print(d)
+# data = GetIndex(pth, state, company_name)
+#
+# e = data.get_index_value()
+# d = data.get_cpn_status()
+#
+# print(e)
+# print(d)
