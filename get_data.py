72156a39495dda083c1f8aece583be1440c28cbc
Quochuy <quochuy3456@gmail.com>
2020-06-10 11:35:42 +0700

    adjuts all files time 5

--- get_data.py
+++ get_data.py
@@ -18,6 +18,10 @@ company_name = "MNB-tong-cong-ty-may-nha-be-cong-ty-co-phan.chn"
 
 
 class GetIndex:
+    """
+        @author Quoc Huy Do
+        get data from cafef.vn of any cpn
+    """
     def __init__(self, _path, state, cpn_name):
         self.path = _path
         self.state = state
@@ -26,32 +30,52 @@ class GetIndex:
         self.get_data()
 
     def get_full_path(self):
+        """
+         Create fullpath
+        @return:
+        """
         return os.path.join(self.path, self.state, self.cpn_name)
 
     def get_data(self):
+        """
+            request data
+        """
         _data = requests.get(self.get_full_path())
         self.all_data = BeautifulSoup(_data.text, "html.parser")
 
     def get_index_value_group_style(self):
+        """
+            get value when eps in group each
+        @return:
+        """
         info = self.all_data.find("div", {"class": "dl-thongtin clearfix"})
         print(info)
         data = info.find_all("ul")[-1]
         li = data.find_all('li')
         dta = []
         for o in li:
-            dta.append(list(map(lambda x: x.get_text(strip=True), o.find_all("div"))))
+            dta.append\
+              (list(map(lambda x: x.get_text(strip=True), o.find_all("div"))))
         return dta
 
     def get_index_value_list_st(self):
+        """
+            get value when eps not in group each
+        @return:
+        """
         info = self.all_data.find("div", {"class": "tidown"})
         lst_val = info.find('ul').find_all("li")
-        print(lst_val)
         iv= []
         for o in lst_val:
-            iv.append(list(map(lambda x: x.get_text(strip=True), o.find_all("span"))))
+            iv.append\
+              (list(map(lambda x: x.get_text(strip=True), o.find_all("span"))))
         return iv
 
     def get_index_value(self):
+        """
+            return company index
+        @return:
+        """
         try:
             index_value = self.get_index_value_group_style()
         except "N":
@@ -60,6 +84,10 @@ class GetIndex:
         return index_value
 
     def get_cpn_status(self):
+        """
+            return companatus
+        @return:
+        """
         info = self.all_data.find("div", {"id": "divHoSoCongTyAjax"})
         table = info.find('table')
         rows = table.find_all('tr')
